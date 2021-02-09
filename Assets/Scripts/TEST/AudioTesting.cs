9c0807bb23b039d7d4694cc5d26bc32b99ccbec6
renzobptrt <renzobptrt@gmail.com>
2021-02-09 01:39:16 -0500

    Audio Manager

---
+++ Assets/Scripts/TEST/AudioTesting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTesting : MonoBehaviour
+{
+    public float volume, pitch;
+    public AudioClip[] sfx;
+    public AudioClip[] music;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.instance.PlaySfx(sfx[0], volume, pitch);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            AudioManager.instance.PlayMusic(music[Random.Range(0,music.Length)]);
+        }
+    }
+}
